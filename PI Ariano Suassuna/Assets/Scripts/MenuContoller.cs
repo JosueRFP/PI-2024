4ea536d70f09fd28ac625b3fbbf7e8fb31036dcb
JosueRFP <josue.petri.senac@gmail.com>
2024-05-26 14:47:52 -0300

    Logica menu concluida

---
+++ PI Ariano Suassuna/Assets/Scripts/MenuContoller.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class MenuContoller : MonoBehaviour
+{
+    public GameObject creditsPainel, controlsPainel, levelPainel;
+
+    public void OpenLevelBTN()
+    {
+        levelPainel.SetActive(true);
+    }
+    public void CloseLevelBTN()
+    {
+        levelPainel.SetActive(false);
+    }
+    
+    public void OpenCreditsBTN()
+    {
+        creditsPainel.SetActive(true);
+    }
+    public void CloseCreditsBTN()
+    {
+        creditsPainel.SetActive(false);
+    } 
+    
+    public void OpenControlsBTN()
+    {
+        controlsPainel.SetActive(true);
+    }
+    public void CloseControlsBTN()
+    {
+        controlsPainel.SetActive(false);
+    }
+
+    public void QuitBTN()
+    {
+        Application.Quit();
+        Debug.Log("Quitou");
+    }
+}
