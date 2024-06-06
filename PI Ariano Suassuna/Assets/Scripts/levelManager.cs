7fa5d83d2d147947764ae930e9555c9b00b46edd
JosueRFP <josue.petri.senac@gmail.com>
2024-06-06 16:38:05 -0300

    Mudanças que eu não me lembro

--- PI Ariano Suassuna/Assets/Scripts/levelManager.cs
+++ PI Ariano Suassuna/Assets/Scripts/levelManager.cs
@@ -7,6 +7,16 @@ using UnityEngine.UI;
 
 public class levelManager : MonoBehaviour
 {
+    public Button[] botões;
+
+    private void Update()
+    {
+        for (int i = 0; i< botões.Length; i = i++)
+        {
+           
+        }
+    }
+    
     public void callLavels()
     {
         SceneManager.LoadScene(PlayerPrefs.GetInt("fase Atual") + 1);
