8c2751c71e683da25c243f053ad437ac8597f58b
JosueRFP <josue.petri.senac@gmail.com>
2024-09-19 16:15:14 -0300

    Player pode "assustar" os cactos

--- PI Ariano Suassuna/Assets/Scripts/EnemyRun.cs
+++ PI Ariano Suassuna/Assets/Scripts/EnemyRun.cs
@@ -7,17 +7,50 @@ public class EnemyRun : MonoBehaviour
     public Transform Player;
     public bool correrDoJogador = false;
     public float detectorDaDistancia;
-    public float velEvasao;
-    public float distEvasao;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float velEvasao = 5f;
+    public float distEvasao = 5f;
+    public float tempEvasao = 5f;
+    public bool estaEvadindo = false;
 
+    private Vector2 direcaoDaEvasao;
+    private float tempDeEvasao = 0f;
+    
+    
     // Update is called once per frame
     void Update()
     {
+        float distEvasao = Vector2.Distance(transform.position, Player.position);
+        if(distEvasao <= detectorDaDistancia && !estaEvadindo)
+        {
+            StartEvadindo();
+        }
+        if (estaEvadindo)
+        {
+            EvadindoMov();
+        }
+    }
+
+    void StartEvadindo()
+    {
+        estaEvadindo = true;
+        tempDeEvasao = tempEvasao;
+
+        //Determina a direção oposta ao jogodor
+        direcaoDaEvasao = (transform.position - Player.position).normalized;
+    }
+
+    void EvadindoMov()
+    {
+        tempDeEvasao -= Time.deltaTime;
         
+        if(tempDeEvasao <= 0)
+        {
+            estaEvadindo = false;
+            return;
+        }
+
+        // Move na direção oposta ao jogador
+        transform.Translate(direcaoDaEvasao * velEvasao * Time.deltaTime);
+
     }
 }
