7d9df46315342d443a3e328b8070ebab4118faf5
JosueRFP <josue.petri.senac@gmail.com>
2024-09-25 08:41:42 -0300

    Spawn dos Inimigos

---
+++ PI Ariano Suassuna/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    //public string spawnEnemy[];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
