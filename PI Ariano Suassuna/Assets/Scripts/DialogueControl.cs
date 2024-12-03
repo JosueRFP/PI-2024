b0ced952b33f9fa31af6955a7273a3c7e4e17251
alicia <alicia.santos1936n@gmail.com>
2024-12-03 14:37:30 -0300

    continuação do dialogo

---
+++ PI Ariano Suassuna/Assets/Scripts/DialogueControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+
+    [Header("Components")]
+    public GameObject dialogueObj;
+    public Image profile;
+    public Text speechText;
+    public Text actorNameText;
+
+    [Header("Settings")]
+    public float typingSpeed;
+
+    
+}
